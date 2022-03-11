fde303903e67438523481927d3a409d93dbc30de
Komal <koarora6@gmail.com>
2022-03-11 07:48:55 -0600

    practice language features

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -8,14 +8,32 @@ namespace LanguageFeatures.Controllers
     {
        public ViewResult Index()
         {
+
+            Product[] productArray =
+            {
+                new Product {Name = "kayak", Price = 275M},
+                new Product{ Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+
+            decimal arrayTotal = productArray.FilterByPrice(20).TotalPrices();
+
+            ShoppingCart cart = new ShoppingCart()
+            {
+                Products = Product.GetProducts()
+            };
+
+            //extension method called as any other normal method
+            decimal cartTotal = cart.TotalPrices();
+
             List<string> results = new List<string> ();
             foreach(Product p in Product.GetProducts())
             {
                 string name = p?.Name ?? "<No Name>";
                 decimal? price = p?.Price;
                 string relatedName = p?.Related?.Name ?? "<None>";
-                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}"
-                    , name, price, relatedName));
+                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
             }
 
             return View(results);
---
+++ LanguageFeatures/LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        //the first param should be the class we are extending
+        public static decimal TotalPrices(this IEnumerable<Product> products)
+        {
+            decimal total = 0;
+            foreach(Product prod in products)
+            {
+                total += prod?.Price ?? 0;
+            }
+
+            return total;
+        }
+
+        public static IEnumerable<Product> FilterByPrice(
+            this IEnumerable<Product> productEnum, decimal minPrice)
+        {
+            foreach(Product prod in productEnum)
+            {
+                if((prod?.Price ?? 0) >= minPrice)
+                {
+                    yield return prod;
+                }
+            }
+        }
+        
+    }
+}
