12ea842e414dcc73ee401bd168ce328274007921
Komal <koarora6@gmail.com>
2022-03-18 14:08:28 -0500

    SportStore project initial

---
+++ SportsStore/SportsStore/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        //The price prop has been decorated with the column attribute
+        //to specify the SQL data type that will be used to store values
+        //for this property. Not all C# types map neatly onto SQL types
+        //and this attribute ensures the database uses an appropriate 
+        //type for the application data
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+    }
+}
---
+++ SportsStore/SportsStore/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var ap00p = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseDeveloperExceptionPage();
+app.UseStatusCodePages();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
